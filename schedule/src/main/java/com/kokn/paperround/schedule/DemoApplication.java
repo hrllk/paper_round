0d11c3ea458c4c00d319e195f2a031a57f2fc555
hwimer <hr.kim@infinigru.com>
2022-08-23 23:35:08 +0900

    design a structure

---
+++ schedule/src/main/java/com/kokn/paperround/schedule/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.kokn.paperround.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
