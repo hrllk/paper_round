2529df73a26da66a2ff08a3393d4123331da5db3
hwimer <hr.kim@infinigru.com>
2022-11-15 23:09:17 +0900

    change: modify logging

--- console/src/main/java/com/kokn/paperround/advisor/ErrorCode.java
+++ console/src/main/java/com/kokn/paperround/advisor/ErrorCode.java
@@ -6,9 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    CONFLICT(HttpStatus.CONFLICT, "already exist"),
-    UNOFFICIAL_WORD(HttpStatus.BAD_REQUEST, "it is not official word please check again");
-
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "bad request"),
+    UNOFFICIAL_WORD(HttpStatus.BAD_REQUEST, "it is not official word please check again"),
+    UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "unauthorized token"),
+    CONFLICT(HttpStatus.CONFLICT, "already exist");
     ErrorCode(HttpStatus status, String message) {
         this.status = status;
         this.message = message;
